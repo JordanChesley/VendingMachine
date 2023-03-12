fa16fa635869c25ce9647933ecc019e2f003441c
JordanChesley <jordan.r.chesley@gmail.com>
2023-03-12 18:52:51 -0400

    Edited comments

--- VendingMachine.cpp
+++ VendingMachine.cpp
@@ -4,42 +4,67 @@ using namespace std;
 
 void VendingMachine::showChoices(int totalCost)
 {
+  // Output each item in the machine
   cout << "1. Twinkies - $2.50\n";
   cout << "2. Snickers Bar - $1.50\n";
   cout << "3. Pop -Tarts - $1.75\n";
   cout << "4. Granola Bars - $3.25\n";
   cout << "5. Pretzels - $4.10\n";
+
+  // Use printf to format the totalCost float
   printf("0. Checkout - $%.2f \n", totalCost / 100.00);
+
+  // Print exit option
+  cout << "-1 - Exit\n";
 }
 
 void VendingMachine::getCustomerChoice(int &userChoice)
 {
-  cout << "Make a choice: ";
-  cin >> userChoice;
+  int input;
+  // Keep asking for their choice if the input does not fall in the range [-1,5].
+  do
+  {
+    cout << "Make a choice: ";
+    cin >> input;
+  } while (input > 6 || input < -1);
+
+  // Overwrite userChoice to the new input.
+  userChoice = input;
 }
 
 int VendingMachine::takeMoney(int totalCost)
 {
+  // If the totalCost is 0, no need to take in money.
   if (totalCost == 0)
     return 0;
   int totalReceived = 0;
   float enteredMoney = 0;
+
+  // Continue to ask for money until the totalReceived is greater than the totalCost.
   do
   {
     printf("Money Entered: %.2f\n", totalReceived / 100.00);
     cout << "Enter money in the following demoninations (1,0.25): ";
     cin >> enteredMoney;
+    // If the money is not 1 or 0.25 denomination, ask again.
     if (enteredMoney != 1.00 && enteredMoney != 0.25)
       continue;
+
+    // Convert to cents
     totalReceived += enteredMoney * 100;
   } while (totalReceived < totalCost);
+
+  // Return the total received.
   return totalReceived;
 }
 
 void VendingMachine::computeChange(int totalCost, int moneyReceived)
 {
+  // Initialize currency,
   int pennies = 0, nickels = 0, dimes = 0, quarters = 0;
   int change = moneyReceived - totalCost;
+
+  // For each coin denomination, calculate the number of each coin type to return.
   while ((change / VendingMachine::QUARTER) > 0)
   {
     quarters++;
@@ -60,5 +85,7 @@ void VendingMachine::computeChange(int totalCost, int moneyReceived)
     pennies++;
     change -= VendingMachine::PENNY;
   }
+
+  // Output the returned change.
   printf("Dispensing Change: $%.2f, Quarters: %d, Dimes: %d, Nickels: %d, Pennies: %d\n", (moneyReceived - totalCost) / 100.00, quarters, dimes, nickels, pennies);
 }
--- main.cpp
+++ main.cpp
@@ -2,50 +2,65 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	VendingMachine vendingMachine;
-	char doAgain;
-	do {
+int main()
+{
+  VendingMachine vendingMachine;
+  char orderAgain;
+  do
+  {
 
-		// Initialize variables to track dynamic information
-		int totalCost = 0;
-		int userChoice = 0;
+    // Initialize variables to track dynamic information
+    int totalCost = 0;
+    int userChoice = 0;
 
-		// Continue to show options and get the user choice while the user is not ready to check out.
-		do {
-			vendingMachine.showChoices(totalCost);
-			vendingMachine.getCustomerChoice(userChoice);
-			switch (userChoice) {
-			case 1:
-				totalCost += 250;
-				break;
-			case 2:
-				totalCost += 150;
-				break;
-			case 3:
-				totalCost += 175;
-				break;
-			case 4:
-				totalCost += 325;
-				break;
-			case 5:
-				totalCost += 410;
-				break;
-			}
-			if (userChoice != 0) totalCost *= 1.07;
-		} while (userChoice != 0);
+    // Continue to show options and get the user choice while the user is not ready to check out.
+    do
+    {
+      vendingMachine.showChoices(totalCost);
+      vendingMachine.getCustomerChoice(userChoice);
+      switch (userChoice)
+      {
 
-		// Have the user enter money into the machine.
-		int moneyRecevied = vendingMachine.takeMoney(totalCost);
+      // Add the price according to their selection.
+      case 1:
+        totalCost += 250;
+        break;
+      case 2:
+        totalCost += 150;
+        break;
+      case 3:
+        totalCost += 175;
+        break;
+      case 4:
+        totalCost += 325;
+        break;
+      case 5:
+        totalCost += 410;
+        break;
+      // If user wishes to exit, end the program.
+      case -1:
+        exit(0);
+      }
 
-		// Return extra change entered into the machine.
-		vendingMachine.computeChange(totalCost, moneyRecevied);
+      // Apply the sales tax.
+      if (userChoice != 0)
+        totalCost *= 1.07;
+    } while (userChoice != 0);
 
-		do {
-			cout << "Do you want to start another order? Y/N ";
-			cin >> doAgain;
-		} while (doAgain != 'Y' && doAgain != 'N');
+    // Have the user enter money into the machine.
+    int moneyRecevied = vendingMachine.takeMoney(totalCost);
 
-	} while (doAgain == 'Y');
-	cout << "Exiting\n";
+    // Return extra change entered into the machine.
+    vendingMachine.computeChange(totalCost, moneyRecevied);
+
+    // Ask if they would like to make another order.
+    do
+    {
+      cout << "Do you want to start another order? Y/N ";
+      cin >> orderAgain;
+    } while (orderAgain != 'Y' && orderAgain != 'N');
+
+  } while (orderAgain == 'Y');
+  // No more orders. Exit.
+  cout << "Exiting\n";
 }
